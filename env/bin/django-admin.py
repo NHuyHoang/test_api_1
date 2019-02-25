44f52285f96c757fd108e54cec69d45f61c310f1
HuyHoang <hoang.nguyenh@s3corp.com.vn>
2019-02-25 08:57:12 +0700

    setup on company

--- env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/oscar/Development/python/workspace/test_api_1/env/bin/python3.7
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
